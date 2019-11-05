77e01476a72bbe896d28ab4f2ed11681f6a24855
Ingrid N <inorrmann@gmail.com>
2019-11-05 11:20:26 -0800

    layout of page finished

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// 16-day forecast API https://openweathermap.org/forecast16
+// apikey = ba7557fe357f3ca6d72951aa5899a445
+
+
+
+
+// icons:
+// <i class="fas fa-cloud-showers-heavy"></i>
+// <i class="fas fa-wind"></i>
+// <i class="fas fa-sun"></i>
+// <i class="fas fa-cloud-sun"></i>
+// <i class="fas fa-snowflake"></i>
+// <i class="fas fa-bolt"></i>
